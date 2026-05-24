41589c36e2996c9ff14f953340921ac63d3b7908
KennyDevz <kenneth.kenn2004@gmail.com>
2026-05-24 22:14:16 +0800

    backend: setup

---
+++ dasig-core/dasig-core/src/main/java/edu/cit/dasig_core/DasigCoreApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.dasig_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasigCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasigCoreApplication.class, args);
+	}
+
+}
---
+++ dasig-core/dasig-core/src/test/java/edu/cit/dasig_core/DasigCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.dasig_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasigCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
